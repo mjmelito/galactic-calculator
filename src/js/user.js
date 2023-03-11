47169482c8dbfd683002a8690d44a4dca3a81c57
Matt Melito <matt_melito@outlook>
2023-03-11 12:52:57 -0800

    Add formula for yearsUntil() method with passing test (Green phase).

--- src/js/user.js
+++ src/js/user.js
@@ -1,14 +1,18 @@
 export default class User {
   constructor(name, age,) {
     this.name = name;
-    this.age = age;
-    this.mercuryAge = parseFloat((this.age * .24).toFixed(2));
-    this.venusAge = parseFloat((this.age * .62).toFixed(2));
-    this.marsAge = parseFloat((this.age * 1.88).toFixed(2));
-    this.jupiterAge = parseFloat((this.age * 11.86).toFixed(2));
-    this.saturnAge = parseFloat((this.age * 29.48).toFixed(2));
-    this.uranusAge = parseFloat((this.age * 84.07).toFixed(2));
-    this.neptuneAge = parseFloat((this.age * 164.9).toFixed(2));
+    this.earthAge = age;
+    this.mercuryAge = parseFloat((this.earthAge * .24).toFixed(2));
+    this.venusAge = parseFloat((this.earthAge * .62).toFixed(2));
+    this.marsAge = parseFloat((this.earthAge * 1.88).toFixed(2));
+    this.jupiterAge = parseFloat((this.earthAge * 11.86).toFixed(2));
+    this.saturnAge = parseFloat((this.earthAge * 29.48).toFixed(2));
+    this.uranusAge = parseFloat((this.earthAge * 84.07).toFixed(2));
+    this.neptuneAge = parseFloat((this.earthAge * 164.9).toFixed(2));
   }
 
+    yearsUntil(birthday) {
+      let futureYearDifference = birthday - this.earthAge;
+      return futureYearDifference;
+    }
 }
