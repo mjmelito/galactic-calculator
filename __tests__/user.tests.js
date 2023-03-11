06e1cb249251ea55e1fc9876959e8d8dbc5d022d
Matt Melito <matt_melito@outlook>
2023-03-11 15:41:19 -0800

    Add test for combining planetYears() methods with yearsUntil() method with good fail (Red phase).

--- __tests__/user.tests.js
+++ __tests__/user.tests.js
@@ -22,18 +22,25 @@ describe('User', () => {
 
   test('should return age in mercury years', () => {
     expect(user1.mercuryYears(user1.earthAge)).toEqual(124.43);
-  })
+  });
 
   test('should return age in venus years', () => {
     expect(user1.venusYears(user1.earthAge)).toEqual(48.67);
-  })
+  });
 
   test('should return age in mars years', () => {
     expect(user1.marsYears(user1.earthAge)).toEqual(15.94);
-  })
+  });
 
   test('should return age in jupiter years', () => {
     expect(user1.jupiterYears(user1.earthAge)).toEqual(2.54);
-  })
+  });
+
+  test('should calculate years until future birthday in planet years', () => {
+    expect(user1.mercuryYears(user1.yearsUntil())).toEqual(41.48);
+    expect(user1.venusYears(user1.yearsUntil())).toEqual(16.22);
+    expect(user1.marsYears(user1.yearsUntil())).toEqual(5.31);
+    expect(user1.jupiterYears(user1.yearsUntil())).toEqual(0.84);
+  });
 })
 
