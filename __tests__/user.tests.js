3b629c90f6d42c0fb9361db63101efb35173056b
Matt Melito <matt_melito@outlook>
2023-03-11 14:11:45 -0800

    Refactor code to correct mistake in formulas, add test for mercuryYears() with good fail (Red phase).

--- __tests__/user.tests.js
+++ __tests__/user.tests.js
@@ -7,16 +7,9 @@ describe('User', () => {
     user1 = new User('Matt', 30);
   });
 
-  test('should correctly create a user object with name, age, and 4 planetary age attributes', () => {
+  test('should correctly create a user object with name and age', () => {
     expect(user1.name).toEqual('Matt');
     expect(user1.earthAge).toEqual(30);
-    expect(user1.mercuryAge).toEqual(7.2);
-    expect(user1.venusAge).toEqual(18.6);
-    expect(user1.marsAge).toEqual(56.4);
-    expect(user1.jupiterAge).toEqual(355.8);
-    expect(user1.saturnAge).toEqual(884.4);
-    expect(user1.uranusAge).toEqual(2522.1);
-    expect(user1.neptuneAge).toEqual(4947);
   });
 
   test('should calculate how many years until a specific user birthday', () => {
@@ -26,5 +19,9 @@ describe('User', () => {
   test('should calculate how many years have passed since a specific user birthday', () => {
     expect(user1.yearsSince(25)).toEqual(5);
   });
+
+  test('should return age in mercury years', () => {
+    expect(user1.mercuryYears(user1.earthAge)).toEqual(124.4);
+  })
 })
 
