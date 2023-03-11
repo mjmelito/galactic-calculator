54b0c3ac118b39a97f6b38022cf71956636cbfaa
Matt Melito <matt_melito@outlook>
2023-03-11 11:01:28 -0800

    Update test to reflect marsAge with good fail(Red phase).

--- __tests__/user.tests.js
+++ __tests__/user.tests.js
@@ -12,7 +12,7 @@ describe('User', () => {
     expect(user1.age).toEqual(30);
     expect(user1.mercuryAge).toEqual(7.2);
     expect(user1.venusAge).toEqual(18.6);
-    expect(user1.marsAge).toEqual(0);
+    expect(user1.marsAge).toEqual(56.4);
     expect(user1.jupiterAge).toEqual(0);
   });
 
