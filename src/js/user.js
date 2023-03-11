ca429eeb609b04febac26c0148edc558b9b0767d
Matt Melito <matt_melito@outlook>
2023-03-11 10:57:07 -0800

    Update user.js with passing test for venusAge.

--- src/js/user.js
+++ src/js/user.js
@@ -3,7 +3,7 @@ export default class User {
     this.name = name;
     this.age = age;
     this.mercuryAge = parseFloat((this.age * .24).toFixed(2));
-    this.venusAge = 0;
+    this.venusAge = parseFloat((this.age * .62).toFixed(2));
     this.marsAge = 0;
     this.jupiterAge = 0;
   }
