9d812ecb983f53d2aaf3265d34fc565440b91f44
Matt Melito <matt_melito@outlook>
2023-03-11 11:18:39 -0800

    Add formula for user.saturnAge with passing test (Green phase).

--- src/js/user.js
+++ src/js/user.js
@@ -6,7 +6,7 @@ export default class User {
     this.venusAge = parseFloat((this.age * .62).toFixed(2));
     this.marsAge = parseFloat((this.age * 1.88).toFixed(2));
     this.jupiterAge = parseFloat((this.age * 11.86).toFixed(2));
-    this.saturnAge = 0;
+    this.saturnAge = parseFloat((this.age * 29.48).toFixed(2));
     this.uranusAge = 0;
     this.neptuneAge = 0;
   }
