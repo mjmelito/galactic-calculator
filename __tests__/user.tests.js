f4197ea8df595837099e7e5e3c1f884d3414ddc3
Matt Melito <matt_melito@outlook>
2023-03-11 10:07:51 -0800

    Add User constructor with name and age attributes.

---
+++ __tests__/user.tests.js
@@ -0,0 +1,16 @@
+import User from '../src/js/user.js';
+
+describe('User', () => {
+  let user1;
+
+  beforeEach(() => {
+    user1 = new User('Matt', 30);
+  });
+
+  test('should correctly create a user object with name and age attributes', () => {
+    expect(user1.name).toEqual('Matt');
+    expect(user1.age).toEqual(30);
+  });
+
+})
+
